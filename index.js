82a0a8e5152d6c6c835bf1a46d3baa34dbd6eda5
Shoshana Berleant <nope>
2016-01-16 17:01:59 -0800

    (m) fix whitespace

--- index.js
+++ index.js
@@ -6,29 +6,29 @@ module.exports = function (rootDir, children, htmlEl) {
     // find top-level nodes
     var nodesToPrint = []
     children.forEach(function (entry) {
-	    var nodeDirs = makeFileArray(entry.path)
-	    if (nodeDirs.length === (rootDirs.length + 1)) {
-		var isChild = true
-		for (var i = 0; i < rootDirs.length && isChild === true; i++) {
-		    isChild = (nodeDirs[i] === rootDirs[i])
-		}
-		if (isChild === true) nodesToPrint.push(entry)
+	var nodeDirs = makeFileArray(entry.path)
+	if (nodeDirs.length === (rootDirs.length + 1)) {
+	    var isChild = true
+	    for (var i = 0; i < rootDirs.length && isChild === true; i++) {
+		isChild = (nodeDirs[i] === rootDirs[i])
 	    }
-	})
+	    if (isChild === true) nodesToPrint.push(entry)
+	}
+    })
 
-	widget.directory(rootDir, nodesToPrint)
-	widget.appendTo(htmlEl)
-        return widget
+    widget.directory(rootDir, nodesToPrint)
+    widget.appendTo(htmlEl)
+    return widget
 }
 
 
 function makeFileArray(pathName) {
-	var fileArray = pathName.split('/')
-	    while (fileArray[0] === "") {
-	      fileArray.shift() // remove empty items from the beginning
-	  }
-      while (fileArray[fileArray.length - 1] === "") {
-	  fileArray.pop() // remove empty items from the end
-      }
-      return fileArray
+    var fileArray = pathName.split('/')
+    while (fileArray[0] === "") {
+	fileArray.shift() // remove empty items from the beginning
+    }
+    while (fileArray[fileArray.length - 1] === "") {
+	fileArray.pop() // remove empty items from the end
+    }
+    return fileArray
 }
